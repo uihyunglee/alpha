300c265ca28323f4ae9f33e7f22277933d5e8975
uihyunglee <uihyunglee.tech@gmail.com>
2023-08-23 01:03:32 +0900

    Add trans_qis_daily_format() method

--- alpha/database.py
+++ alpha/database.py
@@ -17,7 +17,7 @@ class AlphaDB:
         self.conn = psycopg2.connect(**connect_info)
         
         
-    def get_table_name(self):
+    def get_table_names(self):
         """Return DB table name"""
         sql = "SELECT table_name FROM information_schema.tables WHERE table_schema='public';"
         table_name = pd.read_sql(sql, self.conn)
@@ -41,4 +41,30 @@ class AlphaDB:
         
         df = pd.read_sql(sql, self.conn)
         
-        return df            
+        return df
+    
+    
+    def trans_qis_daily_format(self, daily_df, only_ohlcv=True, only_stock=True):
+        """Transform DB data format to QIS data format"""
+        if ('cddt' in daily_df.columns) or ('open' not in daily_df.columns):
+            raise ValueError("You can only input daily data.")
+
+        df = daily_df.copy()
+        if only_stock:
+            df = df[df['sh7code'].str.contains('A')]
+
+        df['date'] = pd.to_datetime(df['dateint'], format='%Y%m%d')
+        df['symbol'] = df['sh7code']
+
+        atf = dict()
+        if only_ohlcv:
+            atf_keys = df.columns[2:7]
+        else:
+            atf_keys = df.columns[2:16]
+
+        for key in atf_keys:
+            atf[key] = pd.pivot_table(data=df, values=key, index='symbol', columns='date')
+            
+        return atf
+            
+            
