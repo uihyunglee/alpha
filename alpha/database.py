507627ee5791aa614afa77b039f7d6541ac2a00a
uihyunglee <uihyunglee.tech@gmail.com>
2023-08-23 13:00:59 +0900

    Add stock filter and ohlcv filter in get_stock_data method
    - I wanted to import data efficiently from DB.

--- alpha/database.py
+++ alpha/database.py
@@ -22,29 +22,34 @@ class AlphaDB:
         sql = "SELECT table_name FROM information_schema.tables WHERE table_schema='public';"
         table_name = pd.read_sql(sql, self.conn)
         return list(table_name.values.reshape(-1))
-    
-    
-    def get_stock_data(self, table, code=None, start_date=None, end_date=None):
+
+        
+    def get_stock_data(self, table, code=None, start_date=None, end_date=None, only_stock=False, only_ohlcv=False):
         """Return stock data in table"""
         if start_date == None:
             start_date = '0'
         if end_date == None:
             end_date = '3000_00_00'
-        
         start_date = int(re.sub(r'[^0-9]', '', start_date))
         end_date = int(re.sub(r'[^0-9]', '', end_date))
         
+        stock_cond = "(sh7code LIKE 'A%') AND" if only_stock else ''
+        ohlcv_cond = 'dateint, sh7code, open, high, low, close, vol' if only_ohlcv else '*'
+        
         if code == None:
-            sql = f"SELECT * FROM {table} WHERE dateint>='{start_date}' AND dateint<='{end_date}';"
+            sql = f"""SELECT {ohlcv_cond} FROM {table} 
+                        WHERE {stock_cond} dateint BETWEEN '{start_date}' AND '{end_date}';
+                        """
         else:
-            sql = f"SELECT * FROM {table} WHERE sh7code='{code}' AND dateint>='{start_date}' AND dateint<='{end_date}';"
-        
+            sh7code = f"('{code}')" if isinstance(code, str) else tuple(code)
+            sql = f"""SELECT {ohlcv_cond} FROM {table} 
+                        WHERE {stock_cond} (sh7code IN {sh7code}) AND (dateint BETWEEN '{start_date}' AND '{end_date}');
+                        """
         df = pd.read_sql(sql, self.conn)
-        
         return df
     
     
-    def trans_qis_daily_format(self, daily_df, only_ohlcv=True, only_stock=True):
+    def trans_qis_daily_format(self, daily_df, only_ohlcv=False, only_stock=False):
         """Transform DB data format to QIS data format"""
         if ('cddt' in daily_df.columns) or ('open' not in daily_df.columns):
             raise ValueError("You can only input daily data.")
@@ -66,5 +71,4 @@ class AlphaDB:
             atf[key] = pd.pivot_table(data=df, values=key, index='symbol', columns='date')
             
         return atf
-            
-            
+    
