30901f9ea3a7694ecc4879a6edd0abaf5e14b0c7
uihyunglee <uihyunglee.tech@gmail.com>
2023-08-27 14:22:50 +0900

    test

--- alpha/backtest/backtest.py
+++ alpha/backtest/backtest.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 plt.rcParams['font.family'] = 'Malgun Gothic'
-plt.rcParams['axes.unicode_minus'] = False
+plt.rcParams['axes.unicode_minus'] = True
 
 
 class Backtest:
