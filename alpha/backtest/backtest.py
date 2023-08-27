7ddd2aa492f364ecb7d53512a55fd1c38265da6f
uihyunglee <uihyunglee.tech@gmail.com>
2023-08-27 14:26:57 +0900

    Add Backtest class

---
+++ alpha/backtest/backtest.py
@@ -0,0 +1,17 @@
+from .utils import *
+from .analysis import *
+from .strategy import *
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.family'] = 'Malgun Gothic'
+plt.rcParams['axes.unicode_minus'] = False
+
+
+class Backtest:
+    def __init__(self):
+        data_path = 'C:/Users/uhLee/data/'
+        a = False
+        
