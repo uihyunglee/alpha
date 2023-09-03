2d4148931ff3a650903f9367aa4d0450e041afa0
uihyunglee <uihyunglee.tech@gmail.com>
2023-09-03 22:12:03 +0900

    Add get_krx_content function
    - If you input generate parameter, return corresponding data

--- alpha/backtest/utils.py
+++ alpha/backtest/utils.py
@@ -1,5 +1,20 @@
 import pandas as pd
 import numpy as np
+import requests
+from io import BytesIO
+
+
+def get_krx_content(self, otp_params):
+    """krx crawling format: Return data corresponding to otp_params"""
+    otp_url = 'http://data.krx.co.kr/comm/fileDn/GenerateOTP/generate.cmd'
+    otp = requests.post(otp_url, headers=self.header, data=otp_params).text
+    
+    down_url = 'http://data.krx.co.kr/comm/fileDn/download_csv/download.cmd'
+    down_params = {"code": otp}
+    down_content = requests.post(down_url, headers=self.header, params=down_params).content
+
+    df = pd.read_csv(BytesIO(down_content), encoding='EUC-KR')
+    return df
 
 
 def krx_code():
@@ -14,3 +29,4 @@ def krx_code():
 def condition_statistic(self, condition):
     condition.sum().plot(figsize = (10,5), title = '일별 시그널 발생 횟수');
     print(condition.sum().describe())
+    
