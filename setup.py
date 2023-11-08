215ff2c5cbf5efc0f19b19edff38bbda2ebcf552
Daniel Park <surfhawk@naver.com>
2023-11-09 00:09:05 +0900

    DEPLOY: Version up to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='alpha',
-    version='0.0.1',
+    version='0.0.2',
     description='[SURI] Alpha-study library',
     author='uihyunglee,surfhawk',
     author_email='uihyunglee.tech@gmail.com',
